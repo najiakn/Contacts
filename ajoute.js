1a1d34d77981ab32075bacb5696f24bb3a842905
unknown <votre@email.com>
2024-04-01 05:57:42 +0000

    form de validation

--- ajoute.js
+++ ajoute.js
@@ -1,74 +1,65 @@
-let inputNom=document.querySelector("[name='nom']");
-let inputBio=document.querySelector("[name='bio']");
-let inputMail=document.querySelector("[name='mail']");
-let inputTele= document.querySelector("[name='tele']");
+let inputNom = document.querySelector("[name='nom']");
+let inputBio = document.querySelector("[name='bio']");
+let inputMail = document.querySelector("[name='mail']");
+let inputTele = document.querySelector("[name='tele']");
 
+document.forms[0].onsubmit = function(e) {
+    let nomValid = false;
+    let bioValid = false;
+    let mailValid = false;
+    let phoneValid = false;
 
-document.forms[0].onsubmit=function(e){
+    console.log(inputNom.value);
+    console.log(inputNom.value.length);
 
-     let nomValid=false;
-     let bioValid=false;
-     let mailValid=false;
-     let phoneValid=false;
-
-
-    
-console.log(inputNom.value);
-console.log(inputNom.value.length);
-
-if(inputNom.value !=="" &&  inputNom.value.length>=5  && inputNom.value.length<=30){
-nomValid=true;
-console.log("vaalide nom")
-}
-
-if(inputBio.value !=="" &&  inputBio.value.length>=2  && inputBio.value.length<=100){
-    bioValid=true;
-    console.log("valide bio");
+    if (
+        inputNom.value !== "" &&
+        inputNom.value.length >= 5 &&
+        inputNom.value.length <= 30 &&
+        inputNom.value.includes(" ")
+    ) {
+        nomValid = true;
+        console.log("vaalide nom");
     }
 
+    if (
+        inputBio.value !== "" &&
+        /^[a-zA-Z]+$/.test(inputBio.value) && // Vérifie si la bio ne contient que des caractères alphabétiques
+        inputBio.value.length >= 2 &&
+        inputBio.value.length <= 100
+    ) {
+        bioValid = true;
+        console.log("valide bio");
+    }
 
-if(inputMail.value !=="" &&  inputMail.value.length>=5  && inputMail.value.length<=100){
-     mailValid=true;
-     console.log("valide bio");
-     }
+    if (
+        inputMail.value !== "" &&
+        inputMail.value.length >= 5 &&
+        inputMail.value.length <= 100
+    ) {
+        mailValid = true;
+        console.log("valide mail");
+    }
 
-if(inputTele.value !=="" &&  inputTele.value.length>=5  && inputTele.value.length<=100){
-          phoneValid=true;
-          console.log("valide bio");
-          }
+    if (
+        inputTele.value !== "" &&
+        inputTele.value.length >= 5 &&
+        inputTele.value.length <= 100
+    ) {
+        phoneValid = true;
+        console.log("valide tele");
+    }
 
- if(nomValid == false || bioValid== false || mailValid==false || phoneValid ==false){
+    if (
+        nomValid == false ||
+        bioValid == false ||
+        mailValid == false ||
+        phoneValid == false
+    ) {
         e.preventDefault();
-     }
-    
-}
+    }
+};
 document.write("hey");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // document.links[0].onclick=function(event){
 
